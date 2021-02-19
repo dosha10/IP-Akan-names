29df37e97e7e09c49eb8e8ed18cad29ad0511b0d
Francis <ndungufrank01@gmail.com>
2021-02-19 15:17:35 +0300

    commit

--- main.js
+++ main.js
@@ -1,2 +1,4 @@
 var CC, YY, MM, DD, d, dayValue;
 var dayNames = ["Sunday", "Monday", "Wednesday", "Thursday", "Friday", "Saturaday"];
+var maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+var femaleNames = ["Akosua", "Adwoa", "Abenaa", "Yaa", "Ama"];
