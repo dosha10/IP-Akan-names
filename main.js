5ff48ee6c89e54ec0e92cb9cca61337180ac77bf
Francis <ndungufrank01@gmail.com>
2021-02-19 18:56:29 +0300

    commit

--- main.js
+++ main.js
@@ -1,8 +1,17 @@
-var CC, YY, MM, DD, d, dayValue;
+var CC, YY, MM, DD, d, dayValue, newName;
+var result = document.getElementById("result")
 var dayNames = ["Sunday", "Monday", "Wednesday", "Thursday", "Friday", "Saturaday"];
 var maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
-var femaleNames = ["Akosua", "Adwoa", "Abenaa", "Yaa", "Ama"];
-var getGender
+var femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+var getGender = () => {
+    if (document.getElementById("male").checked) {
+        return document.getElementById("male").value;
+    }
+    if (document.getElementById("female").checked) {
+        return document.getElementById("female").value;
+    }
+    return false;
+};
 
 var myForm = document.querySelector("#myform")
 myForm.addEventListener("submit", function(e) {
@@ -11,11 +20,17 @@ myForm.addEventListener("submit", function(e) {
     let dobArray = DOB.split("-");
     YY = dobArray[0];
     MM = dobArray[1] - 1;
-    DD = bodArray[2];
+    DD = dobArray[2];
     var date = new Date(YY, MM, DD);
     dayValue = date.getDay();
-    if
-
+    if (getGender() === "male") {
+        newName = maleNames[dayValue]
+        result.innerText = "your new name" + newName
+    }
+    if (getGender() === "female") {
+        newName = femaleNames[dayValue]
+        result.innerText = "your new name" + newName
+    }
 
 
 
